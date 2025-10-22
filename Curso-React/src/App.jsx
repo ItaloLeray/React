0e749b6c4e04b0f53724c7b21ea2ec38638b9169
Italo Adriano <italogleray@gmail.com>
2025-10-22 12:21:11 -0300

    React

---
+++ Curso-React/src/App.jsx
@@ -0,0 +1,66 @@
+import './App.css'
+
+// no react, componentes são FUNÇÕES
+
+// propr é um OBJETO
+// props.children
+
+function TituloFormulario(props) {
+  return (
+    <h2>
+      {props.children}
+    </h2>
+  )
+}
+
+function CampoDeFormulario({ children }) {
+  return (
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+
+//function Label({ children, htmlFor }) {
+function Label(props)  {
+  return (
+    <label htmlFor={htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+function FormularioDeEventos() {
+  return (
+    <form className='form-evento'>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+
+
+      <CampoDeFormulario>
+            <Label htmlFor="nome">
+              Qual o nome do evento?
+            </Label>
+            <input type="text" id='nome' placeholder='Summer dev hits' />
+      </CampoDeFormulario>
+    </form>
+    
+  )
+}
+
+function App() {
+  return (
+   <main>
+    <header>
+      <img src="/logo.png" alt="logo" />
+    </header>
+    <section>
+      <img src="/banner.png" alt="banner" />
+    </section>
+    <FormularioDeEventos />
+   </main>
+  )
+}
+
+export default App
